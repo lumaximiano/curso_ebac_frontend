5790324f595ec135a5e032d80562471aee6f9679
Lucas Maximiano <lukas-maximiano@hotmail.com>
2026-04-24 00:49:58 -0300

    corrige testes: isolamento e independência entre cenários

--- cypress/e2e/agenda-contatos.cy.js
+++ cypress/e2e/agenda-contatos.cy.js
@@ -4,23 +4,25 @@ describe('Testes da Agenda de Contatos', () => {
     cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
   })
 
-  it('Deve adicionar 3 contatos', () => {
+  it('Deve adicionar um contato', () => {
     cy.contains('button', 'Adicionar').click()
     cy.get('input[placeholder="Nome"]').type('EBAC')
     cy.get('input[placeholder="E-mail"]').type('ebac@ebac.com.br')
     cy.get('input[placeholder="Telefone"]').type('11912345678')
     cy.contains('button', 'Adicionar').click()
     cy.contains('EBAC').should('be.visible')
+  })
 
-
+  it('Deve adicionar um segundo contato', () => {
     cy.contains('button', 'Adicionar').click()
     cy.get('input[placeholder="Nome"]').type('Gian Souza')
     cy.get('input[placeholder="E-mail"]').type('gian@ebac.com.br')
     cy.get('input[placeholder="Telefone"]').type('11912345678')
     cy.contains('button', 'Adicionar').click()
     cy.contains('Gian Souza').should('be.visible')
+  })
 
-
+  it('Deve adicionar um terceiro contato', () => {
     cy.contains('button', 'Adicionar').click()
     cy.get('input[placeholder="Nome"]').type('Gian Souza 2')
     cy.get('input[placeholder="E-mail"]').type('gian@ebac.com.br')
@@ -29,7 +31,16 @@ describe('Testes da Agenda de Contatos', () => {
     cy.contains('Gian Souza 2').should('be.visible')
   })
 
-  it('Deve editar o contato EBAC', () => {
+  it('Deve editar um contato', () => {
+    // Primeiro cria o contato que será editado
+    cy.contains('button', 'Adicionar').click()
+    cy.get('input[placeholder="Nome"]').type('EBAC')
+    cy.get('input[placeholder="E-mail"]').type('ebac@ebac.com.br')
+    cy.get('input[placeholder="Telefone"]').type('11912345678')
+    cy.contains('button', 'Adicionar').click()
+    cy.contains('EBAC').should('be.visible')
+    
+    // Depois edita
     cy.contains('li', 'EBAC')
       .parents('.contato')
       .find('button.edit')
@@ -39,7 +50,16 @@ describe('Testes da Agenda de Contatos', () => {
     cy.contains('EBAC - Escola Britânica de Artes Criativas e Tecnologia').should('be.visible')
   })
 
-  it('Deve remover o contato Gian Souza 2', () => {
+  it('Deve remover um contato', () => {
+    // Primeiro cria o contato que será removido
+    cy.contains('button', 'Adicionar').click()
+    cy.get('input[placeholder="Nome"]').type('Gian Souza 2')
+    cy.get('input[placeholder="E-mail"]').type('gian2@ebac.com.br')
+    cy.get('input[placeholder="Telefone"]').type('11987654321')
+    cy.contains('button', 'Adicionar').click()
+    cy.contains('Gian Souza 2').should('be.visible')
+    
+    // Depois remove
     cy.contains('li', 'Gian Souza 2')
       .parents('.contato')
       .find('button.delete')
